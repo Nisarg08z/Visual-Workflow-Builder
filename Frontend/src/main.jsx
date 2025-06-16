4eda7df6c95bdbb1f9f120979bc29339879f2ce4
Nisarg08z <nisarg693z@gmail.com>
2025-06-16 12:51:00 +0530

    day-5.2

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { UserProvider } from './contexts/UserContext';
 import './index.css';
+import '@xyflow/react/dist/style.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
